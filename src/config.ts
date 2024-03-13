af921b472ef1befe0fb13a79e2ad5bf71fa10a9f
rishiTamoghna17 <tamoghna171099@gmail.com>
2024-03-13 22:46:38 +0530

    first commit

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const BOT_TOKEN = process.env.BOT_TOKEN || '6731596487:AAEeJ-4ns15KTl-fh6dj-U9JFfP7vQluyOE'
+export const BOT_TOKEN = process.env.BOT_TOKEN || ''
 
 export const googleCredential = {
   web: {},
