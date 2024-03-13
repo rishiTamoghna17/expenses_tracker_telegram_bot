cb917ce4ae8be3c84fa97ef6638d294c7d57733f
rishiTamoghna17 <tamoghna171099@gmail.com>
2024-03-13 22:52:52 +0530

    first commit

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,13 @@
 export const BOT_TOKEN = process.env.BOT_TOKEN || ''
 
 export const googleCredential = {
-  web: {},
+  web: {
+    client_id: '',
+    project_id: '',
+    auth_uri: 'https:/auth',
+    token_uri: 'https://',
+    auth_provider_x509_cert_url: 'https://www',
+    client_secret: '',
+    redirect_uris: ['http:n'],
+  },
 }
